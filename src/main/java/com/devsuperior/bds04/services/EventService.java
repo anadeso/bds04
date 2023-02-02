1a84a45418f6186814391d0dbaabd0a99873e408
anadeso <anadeso90@gmail.com>
2023-02-02 08:09:45 -0300

    implementando class para obter(GET) info de city/event e class para inserir info em city

--- src/main/java/com/devsuperior/bds04/services/EventService.java
+++ src/main/java/com/devsuperior/bds04/services/EventService.java
@@ -1,7 +1,24 @@
 package com.devsuperior.bds04.services;
 
+import com.devsuperior.bds04.dto.EventDTO;
+import com.devsuperior.bds04.entities.Event;
+import com.devsuperior.bds04.repositories.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class EventService {
+
+    @Autowired
+    private EventRepository repository;
+
+    public List<EventDTO> findAll() {
+        List<Event> list = repository.findAll(Sort.by("name"));
+        return list.stream().map(x -> new EventDTO(x)).collect(Collectors.toList());
+    }
 }
+
